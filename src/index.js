13c2ab04dd3d9705381225e8b1f81a04b530a12f
LolloTech <LolloTech@mail.com>
2022-01-27 13:57:50 +0100

    Applicazione micro fixes di lint

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-'use strict'
+'use strict';
 
-import { Server } from "./server.js"
-import { Client } from "./client.js"
+import {Server} from './server.js';
+import {Client} from './client.js';
 class Main {
-    constructor() {
-        this.server = new Server();
-        this.client = new Client();
-    }
+	constructor() {
+		this.server = new Server();
+		this.client = new Client();
+	}
 
-    async main() {
-        const result = await this.client.fetch("https://cat-fact.herokuapp.com/facts", "GET");
+	async main() {
+		const result = await this.client.fetch('https://cat-fact.herokuapp.com/facts', 'GET');
 
-        console.log(await result.json());
-        this.server.listen();
-    }
+		console.log(await result.json());
+		this.server.listen();
+	}
 }
 
 await new Main().main();
